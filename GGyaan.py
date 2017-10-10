0c96061376768b9cc5aa8213b189aaa779fda69d
AdithyaKamath <adithyakamath@gmail.com>
2017-10-11 00:05:07 +0530

    added IC, company databases with additional functionality

--- GGyaan.py
+++ GGyaan.py
@@ -19,6 +19,8 @@ app.config.update(dict(
 
 ))
 
+
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '1234567890'
 app.config['MYSQL_DATABASE_DB'] = 'dbms'
@@ -29,13 +31,40 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 mysql.init_app(app)
 
 session=dict()
+
+
+@app.context_processor
+def inject_user():
+    return dict(session=session)
+
 session['username']=""
+session['type']=""
+session['company']=list()
+
+
+def companyinit():
+	session['company']=list()
+
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	
+	cursor.execute('''SELECT * from COMPANY''')
+	a = cursor.fetchall()
+	if a is not None:
+		for i in a:
+			session['company'].append(i)
+			
+	else:
+		pass
+	cursor.close()
+	conn.close()
 
 
 @app.route('/', methods = ['GET','POST'])
 @app.route('/login', methods = ['GET','POST'])
 def login():
 	if session['username']:
+		companyinit()
 		return render_template('home.html')
 	return render_template('index.html', flag=False)
 
@@ -44,7 +73,7 @@ def login():
 def register():
 	if request.method == 'POST': #and form.validate()
 		conn = mysql.connect()
-		cursor = conn.cursor()
+		cursor = conn.cursor()		
 		
 		fname = "\'"+str(request.form['fname'])+"\'";
 		lname = "\'"+str(request.form['lname'])+"\'";
@@ -54,8 +83,15 @@ def register():
 		sucess = True;
 		try:
 			cursor.execute(''' INSERT INTO USERBASE values( '''+fname+','+lname+','+rno	+','+email+','+password+''')''')
-			session['username']=request.form['email']
+			session['username']=request.form['rno']
+			session['type']='student'
+			companyinit()
+
 			conn.commit()
+			cursor.close()
+			conn.close()
+			return render_template('home.html')
+
 		except:
 			sucess = False;
 		cursor.close()
@@ -66,6 +102,7 @@ def register():
 @app.route('/logout', methods = ['GET','POST'])
 def logout():
 		session['username']=""
+		session['type']=""
 		return render_template('index.html',flag=False)
 
 @app.route('/authenticate', methods = ['GET','POST'])
@@ -73,19 +110,142 @@ def authenticate():
 	conn = mysql.connect()
 	cursor = conn.cursor();
 
-	email = "\'"+str(request.form['email'])+"\'"
+	rno = "\'"+str(request.form['rno'])+"\'"
 	password = "\'"+str(request.form['passw'])+"\'"
 
-	cursor.execute('''SELECT fname from USERBASE WHERE email='''+email+''' AND password=  '''+ password)
+	cursor.execute('''SELECT fname from USERBASE WHERE rno='''+rno+''' AND password=  '''+ password)
 	a = cursor.fetchone()
 	if a is not None:
 		for i in a:
-			session['username']=request.form['email']
+			session['username']=request.form['rno']
+			session['type']='student'
+			companyinit()
+
 			return render_template('home.html')
 	else:
-		return "FAILED!"
+		cursor.execute('''SELECT * from IC WHERE username='''+rno+''' AND passwd=  '''+ password)
+		a = cursor.fetchone()
+		if a is not None:
+			for i in a:
+				session['username']=request.form['rno']
+				session['type']='ic'
+				companyinit()
+
+			return render_template('home.html')
+		else:
+			return "FAILED!"
 
 	return str(a);
+	
+	
+	
+@app.route('/profile', methods = ['GET','POST'])
+def profile():
+	return render_template('profile.html')
+		
+	
+	
+	
+	
+	
+
+
+
+@app.route('/profilefill',methods = ['GET','POST'])
+def profilefill():
+	if request.method == 'POST': #and form.validate()
+		conn = mysql.connect()
+		cursor = conn.cursor()		
+		
+		
+		address = "\'"+str(request.form['addr'])+"\'";
+		cgpa = str(request.form['cgpa']);
+		resume = "\'"+str(request.form['resume'])+"\'";
+		testexp = "\'"+str(request.form['testexp'])+"\'";
+		
+		
+		if(address==''):
+			address='NULL'
+		if(cgpa==''):
+			cgpa='NULL'
+		if(resume==''):
+			resume='NULL'
+		if(testexp==''):
+			testexp='NULL'
+		
+		
+		
+		cursor.execute('''SELECT rno from STUDENT WHERE rno='''+"\'"+session['username']+"\'")
+		a = cursor.fetchone()
+		
+		try:
+			
+			if a is not None:
+				cursor.execute('''DELETE FROM STUDENT WHERE rno='''+"\'"+session['username']+"\'")
+
+			
+			cursor.execute(''' INSERT INTO STUDENT values( '''+"\'"+session['username']+"\'"+','+address+','+cgpa+','+resume	+','+testexp+''')''')
+			conn.commit()
+		except:
+			pass		
+		cursor.close()
+		conn.close()
+		return render_template('home.html')	
+		
+@app.route('/company', methods = ['GET','POST'])
+def company():
+	return render_template('company.html')
+		
+		
+@app.route('/companyfill',methods = ['GET','POST'])
+def companyfill():
+	if request.method == 'POST': #and form.validate()
+		conn = mysql.connect()
+		cursor = conn.cursor()		
+		
+		
+		cname = "\'"+str(request.form['cname'])+"\'";
+		intake = str(request.form['intake']);
+		stipend = str(request.form['stipend']);
+		
+		benefit = "\'"+str(request.form['benefit'])+"\'";
+		cdate = "\'"+str(request.form['cdate'])+"\'";
+		ctime = "\'"+str(request.form['ctime'])+"\'";
+		
+		if(intake==''):
+			intake='NULL'
+		if(stipend==''):
+			stipend='NULL'
+		if(benefit==''):
+			benefit='NULL'
+		if(cdate==''):
+			cdate='NULL'
+		if(ctime==''):
+			ctime='NULL'
+			
+		cursor.execute('''SELECT cname from COMPANY WHERE cname='''+cname)
+		a = cursor.fetchone()
+
+		try:
+			
+			if a is not None:
+				cursor.execute('''DELETE FROM COMPANY WHERE cname='''+cname)
+
+			
+			cursor.execute(''' INSERT INTO COMPANY values( '''+cname+','+intake+','+stipend+','+benefit+','+cdate+','+ctime+','+"\'"+session['username']+"\'"''')''')
+			conn.commit()
+		except:
+			pass		
+		
+
+
+		
+		cursor.close()
+		conn.close()
+		return render_template('home.html')	
+	
+	
+	
 
 if __name__ == "__main__":
 	app.run()
